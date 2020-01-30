0fe1b5314cfac02d7bcf24b769734f2093e3b70a
Giovanni De Toni <giovanni.det@gmail.com>
2020-01-30 16:42:28 +0100

    Update length sizes when validating quicksort.

--- validation/validate_quicksorting.py
+++ validation/validate_quicksorting.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     if save_results:
         results_file.write('Validation on model: {}'.format(load_path) + ' \n')
 
-    for len in [5, 10, 20, 60, 100]:
+    for len in [5, 20, 60, 100]:
 
         print("** Start validation for len = {}".format(len))
 
